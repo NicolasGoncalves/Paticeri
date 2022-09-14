693c230278df2c8ae79e9ebbba581c8a4d47d36f
Nicolas Gonçalves Bábilas De Marco <nicolasbabilas11@gmail.com>
2022-09-13 22:31:49 -0300

    to tendo q refazer algumas paginas pq o comit tinha ido errado

---
+++ src/pages/landingpage/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+
+export default function LandingPage(){
+
+    return(
+        <main>
+            <section>
+                <h1>Langing Page </h1>
+            </section>
+        </main>
+    );
+};
---
+++ src/pages/loginAdm/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+
+export default function Index(){
+
+    return(
+        <main>
+            <header>
+                <img src='././././public/images/Logo-paticeri' alt=''/>
+            </header>
+
+        </main>
+    );
+};
